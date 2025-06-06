098d05bf64093b85bb2754107721ffa926ce147c
Ishika <ishikajn134@gmail.com>
2025-06-06 21:12:28 +0530

    n

---
+++ starttt/month_1/dsa/bitwise_op.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=4;
+    int b=6;
+    cout<<"a&b="<< (a&b)<<endl; // & is for AND operator
+    cout<<"a|b="<< (a|b)<<endl; // | for or operator
+    cout<<"~a="<< (~a)<<endl; // ~ for not
+    cout<<"a^b="<< (a^b)<<endl; // ^ forr xor
+return 0;
+}
