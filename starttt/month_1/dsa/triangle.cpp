7c3d9c3375c2f6f71434ed567e7ba324ac933deb
Ishika <ishikajn134@gmail.com>
2025-06-17 00:24:41 +0530

    e

--- starttt/month_1/dsa/triangle.cpp
+++ starttt/month_1/dsa/triangle.cpp
@@ -3,11 +3,13 @@ using namespace std;
 int main()
 {
   int n;
+  int num=1;
   cout<<"n?";
   cin>>n;
   for(int i=1;i<=n;i++){    
    for(int j=1;j<=i;j++){
-    cout<<j;
+    cout<<num<<" ";
+    num++;
    }
    cout<<endl;
   }
