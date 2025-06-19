3e6015dc87f40a2475e81724668fe89398fb016e
Ishika <ishikajn134@gmail.com>
2025-06-20 00:19:15 +0530

    p

--- starttt/month_1/projects/turtleDraw.py
+++ starttt/month_1/projects/turtleDraw.py
@@ -2,23 +2,31 @@ from turtle import Turtle, Screen
 import random
 timmu= Turtle()
 timmu.shape("circle")
+timmu.pensize("20")
+timmu.speed("fastest")
 colours=["indigo","deep pink","yellow","blue","lavender","pale violet red"]
-
+movements=[0, 90, 180, 270]
 # for _ in range(20):
 #    timmu.forward(10)
 #    timmu.penup()
 #    timmu.forward(10)
 #    timmu.pendown()
-def draw(numsides):
-    angle=360/numsides
-    for _ in range(numsides):
-      timmu.forward(100)
-      timmu.right(angle)
+# def draw(numsides):
+#     angle=360/numsides
+#     for _ in range(numsides):
+#       timmu.forward(100)
+#       timmu.right(angle)
 
-for side in range(3,11):
+# for side in range(3,11):
+#     timmu.color(random.choice(colours))
+#     draw(side)
+def move(movement):
+    timmu.forward(50)
+    timmu.right(random.choice(movements))
+
+for _ in range(200):
     timmu.color(random.choice(colours))
-    draw(side)
-
+    move(_)
 
 screen=Screen()
 screen.exitonclick()
